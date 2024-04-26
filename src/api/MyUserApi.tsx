d83d415b155726a287434b6578fe9407297dadd6
Phong <ttphong1011@gmail.com>
2024-04-26 16:09:12 +0700

    first cm

---
+++ src/api/MyUserApi.tsx
@@ -0,0 +1,119 @@
+import { User } from "@/type";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useMutation, useQuery } from "react-query";
+import { toast } from "sonner";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+type CreateUserRequest = {
+  auth0Id: string;
+  email: string;
+};
+
+export const useCreateMyUser = () => {
+  const { getAccessTokenSilently } = useAuth0(); // get user access token from auth0 server
+  const createMyUserRequest = async (user: CreateUserRequest) => {
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to create user");
+    }
+  };
+
+  const {
+    mutateAsync: createUser, // a mutate function, initiating a mutation operation to create a new user.
+    isLoading, // properties
+    isError,
+    isSuccess,
+  } = useMutation(createMyUserRequest); //initiate mutation operations, such as creating, updating, or deleting data on the server.
+
+  return {
+    createUser,
+    isLoading,
+    isError,
+    isSuccess,
+  };
+};
+
+type UpdateUserRequest = {
+  name: string;
+  addressLine1: string;
+  city: string;
+  country: string;
+};
+
+export const useUpdateMyUser = () => {
+  const { getAccessTokenSilently } = useAuth0();
+  const updateMyUserRequest = async (formData: UpdateUserRequest) => {
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to update user");
+    }
+    return response.json();
+  };
+  const {
+    mutateAsync: updateUser,
+    isLoading,
+    isSuccess,
+
+    error,
+    reset,
+  } = useMutation(updateMyUserRequest);
+
+  if (isSuccess) {
+    toast.success("User profile updated successfully");
+  }
+  if (error) {
+    toast.error(error.toString());
+    reset();
+  }
+
+  return { updateUser, isLoading };
+};
+
+export const useGetMyUser = () => {
+  const { getAccessTokenSilently } = useAuth0();
+  const getMyUserRequest = async () : Promise<User>=> {
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Failed to fetch user information");
+    }
+    return response.json();
+  };
+
+  const {
+    data: currentUser,
+    isLoading,
+    error,
+  } = useQuery("fetchCurrentUser", getMyUserRequest); // unique key vs function make HTTP request
+
+  if (error) {
+    toast.error(error.toString());
+  }
+
+  return {currentUser, isLoading}
+};
---
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -0,0 +1,141 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import LoadingButton from "@/components/LoadingButton";
+import { User } from "@/type";
+import { useEffect } from "react";
+
+const formSchema = z.object({
+  email: z.string().optional(),
+  name: z.string().min(1, "Name is required"),
+  addressLine1: z.string().min(1, "Address Line 1 is required"),
+  city: z.string().min(1, "City is required"),
+  country: z.string().min(1, "Country is required"),
+});
+
+type userFormData = z.infer<typeof formSchema>; // typeof formSchema is passed as an argument to z.infer (a func)
+
+type Props = {
+  onSave: (userProfileData: userFormData) => void; // void is return type
+  isLoading: boolean; // boolean
+  currentUser: User;
+};
+
+const UserProfileForm = ({ onSave, isLoading, currentUser }: Props) => {
+  const form = useForm<userFormData>({
+    resolver: zodResolver(formSchema), //  assigns the result of zodResolver(formSchema) to the resolver property of the object , zodResolver returns a resolver function using the formSchema
+    defaultValues: currentUser,
+  });
+
+  useEffect(() => {
+    form.reset(currentUser); // sets the form's fields to the values: currentUser
+  }, [currentUser, form]);
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSave)} // using handleSumit from hook instead of defining
+        className="space-y-4 bg-gray-50 rounded-lg md:p-10"
+      >
+        <div>
+          <h2 className="text-2xl font-bold">User Profile Form</h2>
+          <FormDescription>
+            View and change your profile information here
+          </FormDescription>
+        </div>
+        <FormField
+          control={form.control} //  provides the form control methods and state to the FormField component.
+          name="email" // dentify the input field within the form.
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input {...field} disabled className="bg-white" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input {...field} className="bg-white" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div className="flex flex-col md:flex-row  gap-4">
+          <FormField
+            control={form.control}
+            name="addressLine1"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormLabel>Address Line 1</FormLabel>
+                <FormControl>
+                  <Input {...field} className="bg-white" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="city"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>City</FormLabel>
+                <FormControl>
+                  <Input {...field} className="bg-white" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="country"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Country</FormLabel>
+                <FormControl>
+                  <Input {...field} className="bg-white" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        {isLoading ? (
+          <LoadingButton />
+        ) : (
+          <Button type="submit" className="bg-orange-500">
+            Submit
+          </Button>
+        )}
+      </form>
+    </Form>
+  );
+};
+
+export default UserProfileForm;
